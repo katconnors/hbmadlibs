9d55f6727b99ca31d8392459dce99b75918d4809
Katrina Connors <koktakatrina@gmail.com>
2023-12-12 17:00:10 -0800

    created mad libs game

--- madlibs.py
+++ madlibs.py
@@ -3,7 +3,7 @@
 from random import choice
 
 from flask import Flask, render_template, request
-
+#collab with 
 # "__name__" is a special Python variable for the name of the current module.
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
@@ -49,6 +49,32 @@ def greet_person():
     compliment = choice(AWESOMENESS)
 
     return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route("/game")
+def show_madlib_form():
+    """Playing Mad libs"""
+    
+    choice = request.args.get("yeah")
+    
+    if choice == "yes":
+       return render_template("game.html") 
+    else: 
+        return render_template("goodbye.html")
+    
+@app.route("/madlib")
+def show_madlib():
+    
+    """Showing mad libs sentence"""
+
+    color = request.args.get("color")
+    person= request.args.get("person")
+    noun= request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlibs.html", color=color, person=person, adjective=adjective, noun=noun)
+
+    #can't return multiple here
+    #conditional to either load the goodbye html or the game html
 
 
 if __name__ == "__main__":
